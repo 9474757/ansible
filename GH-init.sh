b9213cbe4060343e7f00ad6d45d243827daa6643
root <root@Jnk-master>
2019-09-05 09:44:15 +0000

    first commit

---
+++ GH-init.sh
@@ -0,0 +1,7 @@
+echo "# ansible" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git remote add origin git@github.com:9474757/ansible.git
+git push -u origin master
